391ae7d710c30590317aed326a54b24107b55e92
EC2 Default User <ec2-user@ip-172-31-39-24.ap-northeast-2.compute.internal>
2024-08-13 05:06:00 +0000

    fix: [한재혁] 오류 메시지가 나오지않도록 수정 [SKAIPx]

--- app/main.py
+++ app/main.py
@@ -5,12 +5,18 @@ import colorama
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from deep_learning.controller.deep_learning_controller import deepLearningRouter
 from dice.controller.dice_controller import diceResultRouter
 from system_initializer.init import SystemInitializer
 from task_manager.manager import TaskManager
 from include.socket_server.initializer.init_domain import DomainInitializer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
 
 DomainInitializer.initEachDomain()
 SystemInitializer.initSystemDomain()
@@ -19,6 +25,14 @@ app = FastAPI()
 
 app.include_router(deepLearningRouter)
 app.include_router(diceResultRouter)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     colorama.init(autoreset=True)
